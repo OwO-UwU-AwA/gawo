47e82172343c3fde1112ab8645001dfc3541a45f
mr_magic223325 <mrmagic223325@fedora.email>
2023-03-18 22:53:43 +0100

    Login Form Frontend
    Signed-off-by: Mathis Winterer <mrmagic223325@fedora.email>

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gawo.Pages;
+
+public class LoginModel : PageModel
+{
+    private readonly ILogger<LoginModel> _logger;
+
+    [BindProperty]
+    public string? Username { get; set; }
+
+    [BindProperty]
+    public string? Password { get; set; }
+
+    public LoginModel(ILogger<LoginModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnPost()
+    {
+        Console.WriteLine($"{Username}");
+        Console.WriteLine($"{Password}");
+        if (Username == null || Password == null || Username.Length <= 3 || Password.Length <= 8)
+            return;
+    }
+
+    public void OnGet()
+    {        
+
+    }
+}
---
+++ Pages/Profile.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gawo.Pages;
+
+public class ProfileModel : PageModel
+{
+    private readonly ILogger<ProfileModel> _logger;
+
+    public ProfileModel(ILogger<ProfileModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {        
+        if (HttpContext.User.Identity != null && !HttpContext.User.Identity.IsAuthenticated)
+        {
+            return RedirectToPage("/Login");
+        }
+        else
+            return RedirectToPage("Error");
+    }
+}
