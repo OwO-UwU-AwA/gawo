bb1fcee704ec7d8df2f5d3a5de546efc65d86e10
mr_magic223325 <mrmagic223325@fedora.email>
2023-03-18 12:36:47 +0100

    Initial Commit
    Signed-off-by: Mathis Winterer <mrmagic223325@fedora.email>

---
+++ Pages/Points.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gawo.Pages;
+
+public class PointsModel : PageModel
+{
+    private readonly ILogger<PointsModel> _logger;
+
+    public PointsModel(ILogger<PointsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+      
+    }
+}
