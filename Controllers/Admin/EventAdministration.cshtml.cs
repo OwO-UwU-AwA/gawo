6c23ca3df02df5096b2f136a6a717894511d7b09
Winterer, Mathis Aaron <mrmagic223325@fedora.email>
2024-06-06 07:41:26 +0200

    More work
    Signed-off-by: Winterer, Mathis Aaron <mrmagic223325@fedora.email>

--- Controllers/Admin/EventAdministration.cshtml.cs
+++ Controllers/Admin/EventAdministration.cshtml.cs
@@ -1,14 +1,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SurrealDb.Net;
+using SurrealDb.Net.Models;
 using SurrealDb.Net.Models.Auth;
 
 namespace GaWo.Controllers.Admin;
 
 public class EventAdministrationModel(IAuthorizationService authorizationService) : PageModel
 {
-    public IAuthorizationService AuthorizationService { get; set; } = authorizationService;
-
     public async Task<IEnumerable<Event>> Get()
     {
         var secrets = Secrets.Get().Result;
@@ -27,5 +26,22 @@ public class EventAdministrationModel(IAuthorizationService authorizationService
         db.Dispose();
 
         return res;
+    }
+
+    public async Task<GawoUser> GetUser(Thing thing)
+    {
+        var secrets = Secrets.Get().Result;
+        var db = new SurrealDbClient(Constants.SurrealDbUrl);
+        await db.SignIn(new DatabaseAuth
+        {
+            Namespace = Constants.SurrealDbNameSpace,
+            Database = Constants.SurrealDbDatabase,
+            Username = secrets.Username,
+            Password = secrets.Password
+        });
+
+        var res = await db.Select<GawoUser>(thing);
+
+        return res!;
     }
 }
